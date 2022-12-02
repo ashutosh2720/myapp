6c5af7f46e957698f7d64818219a4f5e8a54bcf9
ASHUTOSH SINGH <ashutoshsinghvns95@gmail.com>
2022-12-02 16:32:27 +0530

    initial commit

--- src/Nav.jsx
+++ src/Nav.jsx
@@ -1,31 +1,65 @@
 import React from "react";
-function Navbar()
-{
-    return(
-       <nav>
-          <div className="nav-left">
-              <img src="https://cdn-icons-png.flaticon.com/512/8336/8336254.png" alt="" /><h3>Menu</h3>
+function Navbar() {
+  return (
+    <>
+      <nav>
+        <div className="nav-left">
+          <img src="https://cdn-icons-png.flaticon.com/512/8336/8336254.png" alt="" /><h3>Menu</h3>
 
-          </div>
-          <div className="nav-mid">
-            <img src="ecom2.png" alt="" />
-          </div>
+        </div>
+        <div className="nav-mid">
+          <img src="ecom2.png" alt="" />
+        </div>
 
-         <div className="nav-right">
-            <div className="img1">
+        <div className="nav-right">
+          <div className="img1">
             <img src="https://cdn-icons-png.flaticon.com/512/149/149852.png" alt="" />
-            </div>
-            <div className="img4">
-                <img src="https://cdn-icons-png.flaticon.com/512/2662/2662503.png" alt="" />
-            </div>
-            <div className="img2">
-            <img src="https://cdn-icons-png.flaticon.com/512/1077/1077035.png" alt="" />
-            </div>
-            <div className="img3">
-            <img src="https://cdn-icons-png.flaticon.com/512/833/833314.png" alt="" />
-            </div>
           </div>
-       </nav>
-    );
+          <div className="img4">
+            <img src="https://cdn-icons-png.flaticon.com/512/2662/2662503.png" alt="" />
+          </div>
+          <div className="img2">
+            <img src="https://cdn-icons-png.flaticon.com/512/1077/1077035.png" alt="" />
+          </div>
+          <div className="img3">
+            <img src="https://cdn-icons-png.flaticon.com/512/833/833314.png" alt="" />
+          </div>
+        </div>
+      </nav>
+
+      <div className="main">
+        <div className="img1">
+          <img src="shop2.png" alt="" />
+        </div>
+         <div className="content">
+          <h1>world is here where are you</h1><br /><br /><br />
+          <h3>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Atque cumque aliquid deleniti, velit blanditiis excepturi iure hic ut voluptatibus provident possimus tempore quas reiciendis reprehenderit exercitationem suscipit nihil expedita accusantium placeat consectetur quam voluptate voluptatem nostrum vel. Tempore deleniti saepe sequi! Ab dicta consectetur sit, provident exercitationem rem quos iure, </h3><br /><br /><br />
+
+          <button>Shop Now</button>
+         </div>
+      </div>
+
+      <div className="items">
+        <div className="item item1">
+          <img src="https://i.pinimg.com/736x/57/4e/a4/574ea4d237a775d63687e2b8ae13661d--mens-fashion.jpg" alt="" />
+        </div>
+        <div className="item item1">
+          <img src="https://www.bing.com/th?id=OIP.thc5Z95GJp3R2Q5822FGywHaLH&w=204&h=306&c=8&rs=1&qlt=90&o=6&dpr=1.6&pid=3.1&rm=2" alt="" />
+        </div>
+        <div className="item item1">
+          <img src="https://th.bing.com/th/id/OIP.C2pYils24WCJ48m43jDRkgHaLH?pid=ImgDet&rs=1" alt="" />
+        </div>
+        <div className="item item1">
+          <img src="https://th.bing.com/th/id/R.cc2c9286a2b8f64a30fdf6142bda6c6c?rik=db%2b3eVtZE0CyOA&riu=http%3a%2f%2fimages4.fanpop.com%2fimage%2fphotos%2f20500000%2fYoon-Shi-Yoon-T-I-For-Men-yoon-shi-yoon-20533459-550-825.jpg&ehk=ifg6eYAkyH7j5r%2f2221Dzm0sGSYvmFWbl75%2bVmq77%2fQ%3d&risl=&pid=ImgRaw&r=0" alt="" />
+        </div>
+        <div className="item item1">
+          <img src="https://th.bing.com/th/id/OIP.-3GBO7ELcxPfqZxMo6wWpgAAAA?pid=ImgDet&w=300&h=400&rs=1" alt="" />
+        </div>
+        
+      </div>
+
+    </>
+
+  );
 };
 export default Navbar;
