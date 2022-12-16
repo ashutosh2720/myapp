82fca9bcdb8949c009945294db0180fb71cd5b2c
ASHUTOSH SINGH <ashutoshsinghvns95@gmail.com>
2022-12-16 15:36:50 +0530

    initial ommit

--- src/Components/Main.jsx
+++ src/Components/Main.jsx
@@ -1,52 +1,12 @@
+import React from "react";
+import slider from "../Images/slide1.svg"
+import '../CSS/main.css'
+
 export default function Main() {
     return (
         <>
-            <div className="main">
-                <div className="img1">
-                    <img src="shop2.png" alt="" />
-                </div>
-                <div className="content">
-                    <h1>world is here where are you</h1><br /><br /><br />
-                    <h3>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Atque cumque aliquid deleniti, velit blanditiis excepturi iure hic ut voluptatibus provident possimus tempore quas reiciendis reprehenderit exercitationem suscipit nihil expedita accusantium placeat consectetur quam voluptate voluptatem nostrum vel. Tempore deleniti saepe sequi! Ab dicta consectetur sit, provident exercitationem rem quos iure, </h3><br /><br /><br />
-
-                    <button>Shop Now</button>
-                </div>
-            </div>
-
-            <div className="items">
-                <div className="item item1">
-                    <img src="https://th.bing.com/th/id/R.109baf9854f45f150e47545e72c7ddea?rik=q%2bORTjYHZML6Lg&riu=http%3a%2f%2f1.bp.blogspot.com%2f-SU3i7C0fIUk%2fUQp2CPnun1I%2fAAAAAAAAAOQ%2fM3g9BGeLLHo%2fs1600%2fLee%2bMin%2bHo%2bPerfect%2bPersonality%2bWallpaper.jpg&ehk=h3RUSgriTYn75nBq8LDg%2b8KPJQCf5h2AXcI%2fQUgpMac%3d&risl=&pid=ImgRaw&r=0" alt="" />
-                </div>
-                <div className="item item1">
-                    <img src="https://www.bing.com/th?id=OIP.thc5Z95GJp3R2Q5822FGywHaLH&w=204&h=306&c=8&rs=1&qlt=90&o=6&dpr=1.6&pid=3.1&rm=2" alt="" />
-                </div>
-                <div className="item item1">
-                    <img src="https://th.bing.com/th/id/OIP.C2pYils24WCJ48m43jDRkgHaLH?pid=ImgDet&rs=1" alt="" />
-                </div>
-                <div className="item item1">
-                    <img src="https://th.bing.com/th/id/R.cc2c9286a2b8f64a30fdf6142bda6c6c?rik=db%2b3eVtZE0CyOA&riu=http%3a%2f%2fimages4.fanpop.com%2fimage%2fphotos%2f20500000%2fYoon-Shi-Yoon-T-I-For-Men-yoon-shi-yoon-20533459-550-825.jpg&ehk=ifg6eYAkyH7j5r%2f2221Dzm0sGSYvmFWbl75%2bVmq77%2fQ%3d&risl=&pid=ImgRaw&r=0" alt="" />
-                </div>
-                <div className="item item1">
-                    <img src="https://th.bing.com/th/id/OIP.-3GBO7ELcxPfqZxMo6wWpgAAAA?pid=ImgDet&w=300&h=400&rs=1" alt="" />
-                </div>
-
-            </div>
-
-            <div className="jacket">
-                <div className="jack mens-jacket">
-                    <img src="https://th.bing.com/th/id/R.109baf9854f45f150e47545e72c7ddea?rik=q%2bORTjYHZML6Lg&riu=http%3a%2f%2f1.bp.blogspot.com%2f-SU3i7C0fIUk%2fUQp2CPnun1I%2fAAAAAAAAAOQ%2fM3g9BGeLLHo%2fs1600%2fLee%2bMin%2bHo%2bPerfect%2bPersonality%2bWallpaper.jpg&ehk=h3RUSgriTYn75nBq8LDg%2b8KPJQCf5h2AXcI%2fQUgpMac%3d&risl=&pid=ImgRaw&r=0" alt="" />
-                </div>
-                <div className="jack mens-jacket-item">
-                    <div className="img1">
-                        <img src="https://i.pinimg.com/originals/e5/6b/79/e56b799b365e63c41041feb38fb7e965.jpg" alt="" />
-                    </div>
-                    <div className="img1">
-                        <img src="https://i.pinimg.com/originals/e5/6b/79/e56b799b365e63c41041feb38fb7e965.jpg" alt="" />
-                    </div>
-                    <div className="img1">
-                        <img src="https://i.pinimg.com/originals/e5/6b/79/e56b799b365e63c41041feb38fb7e965.jpg" alt="" />
-                    </div>
-                </div>
+            <div className="slider">
+           <img src="https://static.vecteezy.com/system/resources/previews/001/213/384/non_2x/online-shopping-mobile-app-with-truck-and-delivery-man-vector.jpg" alt="" />
             </div>
         </>
     )
--- src/Components/Nav.jsx
+++ src/Components/Nav.jsx
@@ -32,10 +32,11 @@ function Navbar() {
             </div>
           </div>
           <div className="hamburger">
-            :
+            <img src="https://cdn-icons-png.flaticon.com/512/1170/1170678.png" alt="" />
           </div>
         </div>
       </nav>
+     
     </>
 
   );
