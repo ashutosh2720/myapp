f6fb715cb521daba4d41c397bc91b377e8506d9f
ASHUTOSH SINGH <ashutoshsinghvns95@gmail.com>
2022-12-17 01:43:27 +0530

    file updated

--- src/Components/Main.jsx
+++ src/Components/Main.jsx
@@ -30,20 +30,24 @@ export default function Main() {
             </div>
             <hr />
             <hr />
-            <div className="cole"> <h2>Our Collectins</h2></div><hr />
+            <div className="cole"> <h2>Our Collections</h2></div><hr />
 
             <div className="collection">
                 <div className="Mens-jacket col">
                     <img src="https://pngimg.com/uploads/leather_jacket/leather_jacket_PNG33.png" alt="" />
+                    <h2>Mens jacket</h2>
                 </div>
                 <div className="Womens-saree col">
                     <img src="https://th.bing.com/th/id/R.c065f741320873055400b4a8f5b43201?rik=tyUE5lyoZkboKw&riu=http%3a%2f%2fwww.pngimagesfree.com%2fPeople%2fSaree%2fSaree-2%2fSilk-Saree-PNG-wearing-model_pngimagesfree.com.png&ehk=P21TJx2eJwbp6GyxN2qfEGBkI7JjapZQTEmlCgGITT0%3d&risl=&pid=ImgRaw&r=0" alt="" />
+                    <h2>Saaree</h2>
                 </div>
                 <div className="Mens-shoes col">
                     <img src="https://pluspng.com/img-png/mens-shoes-hd-png-download-clothes-mens-shoes-2000.png" alt="" />
+                    <h2>Mens Shoes</h2>
                 </div>
                 <div className="girls-lahenga col">
                     <img src="https://cdn.shopify.com/s/files/1/1768/0827/products/MG_9675_grande.png?v=1509561488" alt="" />
+                    <h2>Lahenga</h2>
                 </div>
 
             </div>
