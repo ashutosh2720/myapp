e9cd19f60133dceae6fbd5faabede76f8a68578e
ASHUTOSH SINGH <ashutoshsinghvns95@gmail.com>
2022-12-16 20:36:19 +0530

    initial commit

--- src/Components/Main.jsx
+++ src/Components/Main.jsx
@@ -8,14 +8,14 @@ export default function Main() {
             <div className="slider">
                 <img src="https://static.vecteezy.com/system/resources/previews/001/213/384/non_2x/online-shopping-mobile-app-with-truck-and-delivery-man-vector.jpg" alt="" />
             </div>
-            <div><h1>Best Deal</h1></div>
+            <div className="deal"><h1>Best Deals</h1></div><hr />
             <div className="items">
                 {
                     Items.map((val) => {
                         return (
-                            <div>
+                            <div className="img-details">
                                 <div className="product-img">
-                                    <img style={{ width: '200px' }} src={val.img} alt="" />
+                                    <img src={val.img} alt="" />
                                 </div>
                                 <div className="ind-item">
                                     <h1>{val.title}</h1>
@@ -27,6 +27,7 @@ export default function Main() {
                     })
                 }
             </div>
+            <hr />
         </>
     )
 }
