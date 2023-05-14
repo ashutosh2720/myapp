18cb2f9660e3db35d3bcdd5efb3958fee68938bd
ASHUTOSH SINGH <ashutoshsinghvns95@gmail.com>
2023-05-14 14:18:49 +0530

    footer updated

--- src/Components/footer/Footer.jsx
+++ src/Components/footer/Footer.jsx
@@ -2,7 +2,20 @@ import React from "react";
 import './Footer.css'
 import logo from '../../Images/anix4.png'
 import ShoppingCartOutlinedIcon from '@mui/icons-material/ShoppingCartOutlined';
-
+import FacebookIcon from '@mui/icons-material/Facebook';
+import TwitterIcon from '@mui/icons-material/Twitter';
+import InstagramIcon from '@mui/icons-material/Instagram';
+import GitHubIcon from '@mui/icons-material/GitHub';
+import AddIcCallOutlinedIcon from '@mui/icons-material/AddIcCallOutlined';
+import InfoOutlinedIcon from '@mui/icons-material/InfoOutlined';
+import AccountBalanceWalletOutlinedIcon from '@mui/icons-material/AccountBalanceWalletOutlined';
+import LocalShippingOutlinedIcon from '@mui/icons-material/LocalShippingOutlined';
+import CancelScheduleSendOutlinedIcon from '@mui/icons-material/CancelScheduleSendOutlined';
+import KeyboardReturnOutlinedIcon from '@mui/icons-material/KeyboardReturnOutlined';
+import GavelOutlinedIcon from '@mui/icons-material/GavelOutlined';
+import SecurityOutlinedIcon from '@mui/icons-material/SecurityOutlined';
+import VpnKeyOutlinedIcon from '@mui/icons-material/VpnKeyOutlined';
+import MenuBookOutlinedIcon from '@mui/icons-material/MenuBookOutlined';
 export default function Footer() {
 
     let date = new Date().getFullYear()
@@ -14,10 +27,10 @@ export default function Footer() {
 
                     <ul>
                         <li><h2>About</h2></li>
-                        <li><a href="">Contact Us</a></li>
-                        <li><a href="">About Us</a></li>
-                        <li><a href="">Careers Us</a></li>
-                        <li><a href="">Contact Us</a></li>
+                        <li><a href=""> <AddIcCallOutlinedIcon /> Contact Us</a></li>
+                        <li><a href=""> <InfoOutlinedIcon /> About Us</a></li>
+                        <li><a href=""> <MenuBookOutlinedIcon /> Anix Stories</a></li>
+
 
 
                     </ul>
@@ -25,28 +38,28 @@ export default function Footer() {
                 <div className="help">
                     <ul>
                         <li><h2>Help</h2></li>
-                        <li><a href="">Payment</a></li>
-                        <li><a href="">Shipping</a></li>
-                        <li><a href="">Cancellation & Return</a></li>
-                        <li><a href="">Return</a></li>
+                        <li><a href="" > <AccountBalanceWalletOutlinedIcon /> Payment</a></li>
+                        <li><a href=""> <LocalShippingOutlinedIcon /> Shipping</a></li>
+                        <li><a href=""> <CancelScheduleSendOutlinedIcon /> Cancellation & Return</a></li>
+                        <li><a href=""> <KeyboardReturnOutlinedIcon /> Return</a></li>
                     </ul>
                 </div>
                 <div className="policy">
                     <ul>
                         <li><h2>Policy</h2></li>
-                        <li><a href="">Return Policy</a></li>
-                        <li><a href="">Terms & Use</a></li>
-                        <li><a href="">Privecy</a></li>
-                        <li><a href="">Security</a></li>
+                        <li><a href=""> <KeyboardReturnOutlinedIcon />  Return Policy</a></li>
+                        <li><a href=""> <GavelOutlinedIcon /> Terms & Use</a></li>
+                        <li><a href=""> <SecurityOutlinedIcon />  Privecy</a></li>
+                        <li><a href=""> <VpnKeyOutlinedIcon /> Security</a></li>
                     </ul>
                 </div>
                 <div className="social">
                     <ul>
                         <li><h2>Social</h2></li>
-                        <li><a href="">Facebook</a></li>
-                        <li><a href="">Twitter</a></li>
-                        <li><a href="">Instagram</a></li>
-                        <li><a href="">Github</a></li>
+                        <li><a href=""> <FacebookIcon /> Facebook</a></li>
+                        <li><a href=""> <TwitterIcon /> Twitter</a></li>
+                        <li><a href=""> <InstagramIcon /> Instagram</a></li>
+                        <li><a href=""> <GitHubIcon /> Github</a></li>
                     </ul>
                 </div>
 
